85c5e704788126bcb765443370483d81f863e6e9
ms <>
2026-02-18 11:39:24 +0100

    v1.2.0

--- main.go
+++ main.go
@@ -22,12 +22,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-const (
-	version   = "0.1.0"
-	ansiGreen = "\033[32m"
-	ansiReset = "\033[0m"
-)
-
 func main() {
 	// Define CLI flags
 	var (
---
+++ vars.go
@@ -0,0 +1,7 @@
+package main
+
+const (
+	version   = "1.2.0"
+	ansiGreen = "\033[32m"
+	ansiReset = "\033[0m"
+)
